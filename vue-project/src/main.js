01f3fbdae65ff3c7cff20e3a9ed763005d6f55d6
wendylin84 <wendyl84@nycstudents.net>
2026-03-19 11:02:22 -0400

    er

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 const app = createApp(App)
 
